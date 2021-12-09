8541b227db380e58e7d75a6b31c9fae371ff59a9
VergaraC <victorvaac@al.insper.edu.br>
2021-12-09 01:59:10 -0300

    O

--- tasks/migrations/0001_initial.py
+++ tasks/migrations/0001_initial.py
@@ -14,10 +14,10 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Task',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=200)),
-                ('pub_date', models.DateTimeField(verbose_name='date published')),
-                ('description', models.CharField(max_length=500)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='Id')),
+                ('title', models.CharField(max_length=200, verbose_name='Title')),
+                ('pub_date', models.DateTimeField(verbose_name='Publication Date')),
+                ('description', models.CharField(max_length=500, verbose_name='Description')),
             ],
         ),
     ]
